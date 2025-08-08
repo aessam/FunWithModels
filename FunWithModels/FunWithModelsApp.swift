3cd86d92a24bdb99eb9e76aa1c91027fbf7d84ee
Ahmed Essam <aessam@me.com>
2025-08-08 16:00:41 -0700

    Initial Commit

---
+++ FunWithModels/FunWithModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunWithModelsApp.swift
+//  FunWithModels
+//
+//  Created by Ahmed Essam on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FunWithModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
